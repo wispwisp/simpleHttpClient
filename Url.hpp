e39d1773780a2cb24c597974a8769f967b4f3104
wispwisp <wisp@gcnet.ru>
2015-10-30 14:54:18 +0300

    Url class

---
+++ Url.hpp
@@ -0,0 +1,28 @@
+#ifndef MY_URL
+#define MY_URL
+
+#include <regex>
+#include <string>
+#include <stdexcept>
+
+class Url {
+  std::string m_host;
+  std::string m_res;
+public:
+  Url(const std::string& url) {
+
+    std::regex re{ R"(^(?:http:/+)?([^/]+)(/.*)?$)" };
+    std::smatch match;
+
+    if (!std::regex_match(url, match, re)) {
+      throw std::runtime_error("Invalid url");
+    }
+    
+    m_host = match[1];
+    m_res = match[2].str().empty() ? "/" : match[2].str();
+  }
+  const std::string& host() const { return m_host; }
+  const std::string& resource() const { return m_res; }
+};
+
+#endif // MY_URL
